f657bebc52480c782a04963e9b73bc38c26d98d0
Obinna <>
2020-08-10 15:27:25 +0100

    refactoring - destructuring sfc arguments

--- src/component/common/like.jsx
+++ src/component/common/like.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Like = (props) => {
+const Like = ({ liked, onClick }) => {
   let classes = "heart fa fa-heart";
 
-  if (!props.liked) classes += "-o";
-  return <i onClick={props.onClick} className={classes} aria-hidden="true"></i>;
+  if (!liked) classes += "-o";
+  return <i onClick={onClick} className={classes} aria-hidden="true"></i>;
 };
 
 // class Like extends Component {
